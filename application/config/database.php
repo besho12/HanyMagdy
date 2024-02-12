a51c3562852f758cef76d6de7f216e88cdc31d74
besho12 <40858313+besho12@users.noreply.github.com>
2024-02-12 22:06:18 +0200

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'https://hanymagdy-thelegend.com/',
+	'hostname' => 'localhost',
 	'username' => 'root',
 	'password' => '@@@Hany###200',
 	'database' => 'u817501697_system',
